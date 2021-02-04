56f9f9695c258a0c34af9ba3e2b9ab023a9acc41
kgall <karl.forrest.gall@ibm.com>
2021-02-04 17:11:28 -0500

    api bearer

--- src/api_help/bearer.js
+++ src/api_help/bearer.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 export const getBearer = email => {
+  let mail = "";
   axios
     .post(
       `https://d9u7x85vp9.execute-api.us-east-2.amazonaws.com/production/auth`,
       { email: email }
     )
     .then(function (response) {
-      return response.data.token;
+      mail = response.data.token;
     })
     .catch(function (error) {
       window.alert("Sorry something went wrong.");
     });
+  return mail;
 };
