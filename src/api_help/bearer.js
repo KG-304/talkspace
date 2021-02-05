b41def51b3f69459ccd178e9b1f61ca33a983b24
kgall <karl.forrest.gall@ibm.com>
2021-02-04 22:50:18 -0500

    remove commented out function.

--- src/api_help/bearer.js
+++ src/api_help/bearer.js
@@ -9,19 +9,3 @@ export const getBearer = async email => {
     .then(res => res.data.token)
     .catch(console.log("Sorry an error occurred."));
 };
-
-// export const getBearer = email => {
-//   let mail = "";
-//   axios
-//     .post(
-//       `https://d9u7x85vp9.execute-api.us-east-2.amazonaws.com/production/auth`,
-//       { email: email }
-//     )
-//     .then(function (response) {
-//       mail = response.data.token;
-//     })
-//     .catch(function (error) {
-//       window.alert("Sorry something went wrong.");
-//     });
-//   return mail;
-// };
