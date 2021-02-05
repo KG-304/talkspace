263c098db41de1bf008c984751b95fa1603a7a26
kgall <karl.forrest.gall@ibm.com>
2021-02-05 10:58:09 -0500

    update landing page.

---
+++ src/molecules/landingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../atoms/header";
+import Login from "../atoms/login";
+
+const LandingPage = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+};
+
+export default LandingPage;
