411baf5fa68ba9fa487e369a62b9227cb274d95b
kgall <karl.forrest.gall@ibm.com>
2021-02-04 22:47:09 -0500

    remove bearer

--- src/atoms/header.js
+++ src/atoms/header.js
@@ -4,14 +4,7 @@ import UserContext from "../state_manage/userContext";
 const Header = () => {
   const context = useContext(UserContext);
   return (
-    <>
-      <div style={{ position: "absolute", top: "1px" }}>
-        {context.user.email}
-      </div>
-      <div style={{ position: "absolute", top: "20px" }}>
-        {context.user.bearer}
-      </div>
-    </>
+    <div style={{ position: "absolute", top: "1px" }}>{context.user.email}</div>
   );
 };
 
