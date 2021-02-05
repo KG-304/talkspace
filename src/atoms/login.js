cb7f1a1bd2b556937625bbbaf7378b5377911a1b
kgall <karl.forrest.gall@ibm.com>
2021-02-05 10:57:50 -0500

    style changes, add useHistory

--- src/atoms/login.js
+++ src/atoms/login.js
@@ -4,22 +4,24 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import UserContext from "../state_manage/userContext";
 import { getBearer } from "../api_help/bearer";
+import { Backdrop } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
     "& > *": {
-      margin: "20px",
+      margin: "25px",
       width: "90%",
       height: "100%",
       outline: "none",
     },
   },
-  button: {
-    color: "blue",
-    backgroundColor: "gray",
+  drop: {
+    height: "0",
   },
-  test: {
-    backgroundColor: "white",
+  button: {
+    border: "solid 0.5px black",
+    borderRadius: "10px",
   },
 }));
 
@@ -34,6 +36,7 @@ const modalStyle = {
   outline: "none",
   background: "white",
   textAlign: "center",
+  overflowY: "auto",
 };
 
 const Login = () => {
@@ -42,6 +45,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const context = useContext(UserContext);
   const classes = useStyles();
+  const history = useHistory();
 
   const checkEmail = () => {
     let em = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -52,7 +56,7 @@ const Login = () => {
         context.updateBearer(result);
       });
       context.updateEmail(email);
-      setModal(false);
+      history.push("/game");
     } else {
       setError(true);
     }
@@ -62,16 +66,19 @@ const Login = () => {
     <Modal
       style={modalStyle}
       open={modal}
+      BackdropComponent={Backdrop}
       BackdropProps={{
         classes: {
-          root: classes.test,
+          root: classes.drop,
         },
       }}
     >
       <Fade in={modal}>
         <div className="modal-container">
           <form className={classes.root} noValidate autoComplete="off">
-            <span style={{ marginBottom: "10px" }}>Tic Tac Toe</span>
+            <span style={{ marginBottom: "10px" }}>
+              Welcome, please enter your email.
+            </span>
             <TextField
               error={error}
               value={email}
@@ -79,7 +86,7 @@ const Login = () => {
               variant="filled"
               placeholder="test@email.com"
               type="email"
-              helperText={error ? "Invalid entry" : null}
+              helperText={error ? `Invalid entry: ${email}` : null}
               onChange={event => setEmail(event.target.value)}
             />
             <Button className={classes.button} onClick={() => checkEmail()}>
