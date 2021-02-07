faa2bce3de1290b91de489614fc4eb99d88fbbcb
kgall <karl.forrest.gall@ibm.com>
2021-02-06 20:47:01 -0500

    update game with styles.

--- src/molecules/game.js
+++ src/molecules/game.js
@@ -1,30 +1,56 @@
 import React, { useContext, useState } from "react";
 import Header from "../atoms/header";
-import { getBoard } from "../api_help/board";
+import { getBoard } from "../api_help/api";
 import UserContext from "../state_manage/userContext";
+import Move from "../atoms/move";
+import { Modal, Button } from "@material-ui/core";
+import ReplayIcon from "@material-ui/icons/Replay";
+import { checkBoard } from "../api_help/checkBoard";
 
-const start = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
+const resetStyle = {
+  position: "absolute",
+  top: "40%",
+  left: "150px",
+  fontWeight: "300",
+  cursor: "pointer",
+};
+
+const modalStyle = {
+  position: "fixed",
+  top: "40%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  height: "10vh",
+  width: "40vw",
+  borderRadius: "10px",
+  outline: "none",
+  background: "white",
+  textAlign: "center",
+};
 
 //implement useEffect api for when board changes make API call//
 
 const Game = () => {
-  const [board, setBoard] = useState(start);
+  const [board, setBoard] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+  const [finish, setFinish] = useState(false);
   const context = useContext(UserContext);
 
-  function myFunc(row, col) {
+  function makeMove(row, col) {
     let copy = [...board];
     if (copy[row][col] !== "O" && copy[row][col] !== "X") {
       copy[row][col] = "X";
       getBoard(copy, context.user.bearer).then(function (result) {
-        if (typeof result !== "array") {
-          console.log("Someone won");
+        if (result === undefined) {
+          setFinish(true);
+          setBoard(["", "", ""], ["", "", ""], ["", "", ""]);
+        } else {
+          setBoard(result);
+          checkBoard(result);
         }
-        //Have to call .then() bc otherwise you promise will always be pending.
-        setBoard(result);
       });
     } else {
       window.alert("Sorry invalid move. Please try again.");
@@ -34,46 +60,59 @@ const Game = () => {
   return (
     <>
       <Header />
+      <Button
+        style={resetStyle}
+        onClick={() =>
+          setBoard([
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""],
+          ])
+        }
+      >
+        <ReplayIcon style={{ height: "200px", width: "200px" }} />
+      </Button>
+      <Modal open={finish} style={modalStyle} onClose={() => setFinish(false)}>
+        <span>Loading....</span>
+      </Modal>
       <table>
         <tbody>
           <tr>
-            <td onClick={event => myFunc(0, 0)}>{board[0][0]}</td>
-            <td onClick={() => myFunc(0, 1)} className="vertical-line">
-              {" "}
-              {board[0][1]}
+            <td onClick={() => makeMove(0, 0)}>
+              <Move val={board[0][0]} />
             </td>
-            <td onClick={() => myFunc(0, 2)}> {board[0][2]}</td>
+            <td onClick={() => makeMove(0, 1)} className="vertical-line">
+              <Move val={board[0][1]} />
+            </td>
+            <td onClick={() => makeMove(0, 2)}>
+              <Move val={board[0][2]} />
+            </td>
           </tr>
 
           <tr>
-            <td onClick={() => myFunc(1, 0)} className="horizontal-line">
-              {" "}
-              {board[1][0]}
+            <td onClick={() => makeMove(1, 0)} className="horizontal-line">
+              <Move val={board[1][0]} />
             </td>
             <td
-              onClick={() => myFunc(1, 1)}
+              onClick={() => makeMove(1, 1)}
               className="horizontal-line vertical-line"
             >
-              {board[1][1]}
+              <Move val={board[1][1]} />
             </td>
-            <td onClick={() => myFunc(1, 2)} className="horizontal-line">
-              {board[1][2]}
+            <td onClick={() => makeMove(1, 2)} className="horizontal-line">
+              <Move val={board[1][2]} />
             </td>
           </tr>
 
           <tr>
-            <td id="[2][0]" onClick={() => myFunc(2, 0)}>
-              {board[2][0]}
+            <td onClick={() => makeMove(2, 0)}>
+              <Move val={board[2][0]} />
             </td>
-            <td
-              id="[2][1]"
-              onClick={() => myFunc(2, 1)}
-              className="vertical-line"
-            >
-              {board[2][1]}
+            <td onClick={() => makeMove(2, 1)} className="vertical-line">
+              <Move val={board[2][1]} />
             </td>
-            <td id="[2][2]" onClick={() => myFunc(2, 2)}>
-              {board[2][2]}
+            <td onClick={() => makeMove(2, 2)}>
+              <Move val={board[2][2]} />
             </td>
           </tr>
         </tbody>
